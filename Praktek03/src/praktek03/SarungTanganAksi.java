d8391c7b48ac2d3e8608c3f3836883763e5018ab
MasBayuTriPrasetyo <Bayutp0414@gmail.com>
2018-04-18 14:26:23 +0700

    Getter and Setter

--- Praktek03/src/praktek03/SarungTanganAksi.java
+++ Praktek03/src/praktek03/SarungTanganAksi.java
@@ -6,6 +6,7 @@ public class SarungTanganAksi {
         s.setMerk("oklay");
         s.setWarna("hitam");
         s.setHarga(2000);
+        //Gak Bisa s.setJenisKaosKaki();
         
         
         s.cetakInfo();
@@ -16,6 +17,16 @@ public class SarungTanganAksi {
         System.out.println(s.getWarna());
         System.out.print("Harganya \t: ");
         System.out.println(s.getHarga());
+        
+        KaosKaki kk = new KaosKaki();
+        kk.setMerk("Bilabong");
+        kk.setWarna("Hitam");
+        kk.setHarga(5000);
+        kk.setJenisKaosKaki("Kaos Kaki Panjang");
+        kk.cetakInfo();
+        System.out.println("Jenis \t:"+kk.getJenisKaosKaki());
+        kk.throttle();
+            
         
     }
 }
