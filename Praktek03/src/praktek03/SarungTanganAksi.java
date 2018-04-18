16d3b15dd2d73325889ff98073cfd81d0c2d7c2c
MasBayuTriPrasetyo <Bayutp0414@gmail.com>
2018-04-18 14:01:54 +0700

    instance, buat atribut dan panggil method

--- Praktek03/src/praktek03/SarungTanganAksi.java
+++ Praktek03/src/praktek03/SarungTanganAksi.java
@@ -1,6 +1,12 @@
 package praktek03;
 public class SarungTanganAksi {
     public static void main(String[] args) {
+        SarungTangan s = new SarungTangan();
+        s.merk="oklay";
+        s.warna="hitam";
+        s.harga=2000;
+        
+        s.cetakInfo();
         
     }
 }
