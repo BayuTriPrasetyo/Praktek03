916c5343f6ee8ddd6ca3fcf15b03ab18b7742dcc
MasBayuTriPrasetyo <Bayutp0414@gmail.com>
2018-04-18 14:16:31 +0700

    attribute dan method

--- Praktek03/src/praktek03/KaosKaki.java
+++ Praktek03/src/praktek03/KaosKaki.java
@@ -1,5 +1,9 @@
 package praktek03;
 public class KaosKaki extends SarungTangan{
+    private String jenisKaosKaki;
     
+    void throttle(){
+        System.out.println("Kaos Kaki Bola");
     
+}
 }
