1a05c71cec8d1f72508093ee72dbe6d6181e844e
MasBayuTriPrasetyo <Bayutp0414@gmail.com>
2018-04-18 14:06:59 +0700

    menggunakan method set

--- Praktek03/src/praktek03/SarungTanganAksi.java
+++ Praktek03/src/praktek03/SarungTanganAksi.java
@@ -2,9 +2,11 @@ package praktek03;
 public class SarungTanganAksi {
     public static void main(String[] args) {
         SarungTangan s = new SarungTangan();
-        s.merk="oklay";
-        s.warna="hitam";
-        s.harga=2000;
+        
+        s.setMerk("oklay");
+        s.setWarna("hitam");
+        s.setHarga(2000);
+        
         
         s.cetakInfo();
         
